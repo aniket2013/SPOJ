a5bbf2978a05b51a24ebc072a0456020a734497e
aniket2013 <43495635+aniket2013@users.noreply.github.com>
2018-09-22 18:42:38 +0530

    Create EIGHTS.cpp

---
+++ EIGHTS.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+ 
+    long long int t,k,n;
+    scanf("%lld",&t);
+    while(t--)
+    {
+ 
+        scanf("%lld",&k);
+        n=192+250*(k-1);
+        printf("%lld\n",n);
+    }
+ 
+}
