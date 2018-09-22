79255860a7f663ec18b4a7876096ae21cd4f4c8d
aniket2013 <43495635+aniket2013@users.noreply.github.com>
2018-09-22 18:00:40 +0530

    Create AMR12D.cpp

---
+++ AMR12D.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    scanf("%d", &t);
+    char a[100];
+    int flag;
+    while (t--) {
+        flag = 1;
+        scanf("%s", a);
+        int n = strlen(a) - 1;
+        int i;
+        for (i = 0; i <= (n / 2); i++)
+            if (a[i] != a[n - i]) {
+                flag = 0;
+                break;
+            }
+        if (flag == 0)
+            printf("NO\n");
+        else
+            printf("YES\n");
+    }
+    return (0);
+}
+ 
